ac73c7b35ff8f4e3a252d749539dfb4e764a28fa
KORODEN <47099360+KORODEN@users.noreply.github.com>
2021-09-29 20:13:08 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const board = document.querySelector('#board')
+const SQUARES_NUMBER = 500
+const colors = ['#fe5e31', '#fe9001', '#fed3a5', '#72b8c9', '#5b7c7d']
+
+for(let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+    square.addEventListener('mouseleave', removeColor)
+
+    square.addEventListener('touchmove', setColor)
+    square.addEventListener('touchend', removeColor)
+
+    board.append(square)
+}
+
+function setColor(event) {
+    const element = event.target
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+
+    return colors[index]
+}
